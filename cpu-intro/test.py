4ecc3d1ad5c30981599e5fd66337db65b67e9e50
韩帅虎 <hanhuaishu.@ainfinit.com>
2025-03-11 17:09:44 +0800

    init cm

---
+++ cpu-intro/test.py
@@ -0,0 +1,16 @@
+
+
+import random
+
+random.seed(1)
+
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
